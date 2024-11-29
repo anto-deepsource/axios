5260f27594e0253db0ce9c5b6cf3992a91213002
Anto Christopher <anto@deepsource.io>
2024-11-29 10:49:24 +0530

    ide test 31

--- test/unit/adapters/http.js
+++ test/unit/adapters/http.js
@@ -551,6 +551,8 @@ describe('supports http with nodejs', function () {
       console.log('responseBody', responseBody);
       console.log('responseBody', responseBody);
 
+      console.log('responseBody', responseBody);
+
       for (const [typeName, zipped] of Object.entries({
         gzip: gzip(responseBody),
         compress: gzip(responseBody),
